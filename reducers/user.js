442a7a307f3d9f5c344b0991b4f69e102c18e327
lionel <argv0002@gmail.com>
2024-12-09 17:12:02 +0100

    pull lionel

--- reducers/user.js
+++ reducers/user.js
@@ -1,7 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
- value: [],
+ value: { 
+  token : null, 
+  password: null,
+  email: null,
+  civility: null,
+  name: null,
+  firstname: null,
+  adress: null,
+  city: null,
+  zip: null,
+  phone: null,
+  type: null,
+  children: [],
+},
 };
 
 export const nomDuReducerSlice = createSlice({
@@ -9,8 +22,19 @@ export const nomDuReducerSlice = createSlice({
 
   initialState,
  reducers: {
-   nomFonction : (state, action) => {
-     state.value.push(action.payload);
+   login : (state, action) => {
+     state.value.token = action.payload.token;
+     state.value.password = action.payload.password;
+     state.value.email = action.payload.email;
+     state.value.civility = action.payload.civility;
+     state.value.name = action.payload.name;
+     state.value.firstname = action.payload.firstname;
+     state.value.adress = action.payload.adress;
+     state.value.city = action.payload.city;
+     state.value.zip = action.payload.zip;
+     state.value.phone = action.payload.phone;
+     state.value.type = action.payload.type;
+     state.value.children = action.payload.children;
    },
  },
 });
